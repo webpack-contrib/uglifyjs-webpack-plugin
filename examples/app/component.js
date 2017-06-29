f6c5aa9d20ec13f9f377dcb613ff845b607e8b06
Juho Vepsäläinen <bebraw@gmail.com>
2017-06-29 05:14:19 +0200

    refactor: apply `webpack-defaults` (#35)
    * fix(package): mv uglify2 to `dependencies` && update `peerDependencies` (#45)
    
    * refactor: apply to `webpack-defaults`
    
    * chore(package): update `webpack-defaults v1.0.1...1.4.0`
    
    BREAKING CHANGE: Enforces `peerDependencies` of `"webpack": ">= 3.0.0-rc.0 || ^3.0.0"`.
    
    BREAKING CHANGE: Enforces `engines` of `"node": ">=4.3.0 < 5.0.0 || >= 5.10`

--- examples/app/component.js
+++ examples/app/component.js
@@ -1,8 +1,8 @@
-export default function() {
-	var element = document.createElement("h1");
+export default function () {
+  const element = document.createElement('h1');
 
-	element.className = "demo-button";
-	element.innerHTML = "Hello world";
+  element.className = 'demo-button';
+  element.innerHTML = 'Hello world';
 
-	return element;
+  return element;
 }
