fdea53702ffed8c3f78afabefced66f6912867a5
糖饼 <sugarpie.tang@gmail.com>
2017-07-17 21:28:26 +0800

    refactor: use eslint for serialization checks

--- src/uglify/serialization.js
+++ src/uglify/serialization.js
@@ -1,40 +1,121 @@
 /* eslint-disable
   no-new-func
 */
+import espree from 'espree';
+import { Linter } from 'eslint';
+
 const toType = value => (Object.prototype.toString.call(value).slice(8, -1));
-const TYPES = ['RegExp', 'Function'];
+const formatMessage = ({ source, line, message }) => {
+  const lines = source.split(/\n/);
+  const start = Math.max(line - 3, 0);
+  const end = Math.min(lines.length, line + 3);
+
+  // Error context
+  const context = lines.slice(start, end).map((code, index) => {
+    const number = index + start + 1;
+    const left = number === line ? ' >> ' : '    ';
+    return `${left}${number}| ${code}`;
+  }).join('\n');
+
+  // Alter exception message
+  return `${message}\n${context}`;
+};
+
+const linter = new Linter();
+
+linter.defineRule('no-this', {
+  create(context) {
+    return {
+      ThisExpression(node) {
+        context.report({
+          node,
+          message: 'Unallowed use of `this`',
+        });
+      },
+    };
+  },
+  meta: {
+    docs: {
+      description: 'Forbid the use of `this`.',
+      recommended: 'error',
+    },
+  },
+});
+
 
 export const encode = (key, value) => {
   const type = toType(value);
-  if (TYPES.indexOf(type) !== -1) {
-    return `<${type}|${value.toString()}>`;
+  if (encode[type]) {
+    return `<${type}>${encode[type](value, key)}`;
   }
   return value;
 };
 
+encode.RegExp = value => String(value);
+encode.Function = value => String(value);
+
 export const decode = (key, value) => {
   if (typeof value === 'string') {
-    const reg = /^<(\w+)\|([\w\W]*)>$/;
+    const reg = /^<(\w+)>([\w\W]*)$/;
     const match = value.match(reg);
-    if (match) {
-      if (match[1] === 'Function') {
-        return Function(`
-          try {
-            return (${match[2]}).apply(null, arguments);
-          } catch(err) {
-            if (err instanceof ReferenceError && ~err.message.indexOf('is not defined')) {
-              throw new Error('If the option "${key}" is a function, and it relies on external var, may not work in multi-process mode after serialization: ' + err.message);
-            } else {
-              throw err;
-            }
-          }
-        `);
-      } else if (TYPES.indexOf(match[1]) !== -1) {
-        return Function(`return ${match[2]}`)();
-      }
+    if (match && decode[match[1]]) {
+      return decode[match[1]](match[2], key);
     }
   }
 
   return value;
 };
 
+decode.RegExp = (value, key) => {
+  decode.RegExp.validate(value, key);
+  return Function(`return ${value}`)();
+};
+decode.RegExp.validate = (value, key) => {
+  const ast = espree.parse(`(${value})`);
+
+  if (ast.body[0].expression.type !== 'Literal' && !ast.body[0].expression.regex) {
+    throw new Error(`"${key}" is not a regexp`);
+  }
+};
+
+decode.Function = (value, key) => {
+  decode.Function.validate(value, key);
+  return Function(`return ${value}`)();
+};
+decode.Function.validate = (source, name) => {
+  let ast;
+  const parserOptions = {
+    sourceType: 'module',
+  };
+
+  try {
+    ast = espree.parse(`(${source})`, parserOptions);
+  } catch (err) {
+    throw new Error(`"${name}" parse failed: ${err.message}`);
+  }
+  if (ast.body[0].expression.type !== 'FunctionExpression' && ast.body[0].expression.type !== 'ArrowFunctionExpression') {
+    throw new Error(`"${name}" is not a function`);
+  }
+
+  const messages = linter.verify(`(${source})`, {
+    parserOptions,
+    rules: {
+      'no-undef': 'error',
+      'no-eval': 'error',
+      'no-new-func': 'error',
+      'no-this': 'error',
+    },
+  }, {
+    allowInlineConfig: false,
+  });
+
+  if (messages.length) {
+    const [errors] = messages;
+    const { line, message } = errors;
+    throw new Error(`"${name}" parse failed: ${formatMessage({
+      source,
+      line,
+      message,
+    })}`);
+  }
+};
--- src/uglify/worker.js
+++ src/uglify/worker.js
@@ -2,8 +2,15 @@ import minify from './minify';
 import { decode } from './serialization';
 
 module.exports = (options, callback) => {
+  let minifyOptions;
   try {
-    const result = minify(JSON.parse(options, decode));
+    minifyOptions = JSON.parse(options, decode);
+  } catch (errors) {
+    callback(new Error(`options serialization failed: ${errors.message}`));
+    return;
+  }
+  try {
+    const result = minify(minifyOptions);
     callback(null, result);
   } catch (errors) {
     callback(errors);
