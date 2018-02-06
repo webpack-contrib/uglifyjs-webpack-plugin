fd329a44782d7a1aa1c3ea0da6ffa537bf081505
evilebottnawi <alexander.krasnoyarov@itgalaxy.company>
2018-02-06 18:31:07 +0300

    fix: invalidate cache if source map was changed

--- src/uglify/versions.js
+++ src/uglify/versions.js
@@ -1,5 +1,6 @@
 export default {
   uglify: require('uglify-es/package.json').version, // eslint-disable-line global-require
   plugin: require('../../package.json').version, // eslint-disable-line global-require
+  sourceMap: require('source-map/package.json').version, // eslint-disable-line global-require
 };
 
