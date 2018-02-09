7d99649e96288f9c9424075c6120af9a7c846fb3
evilebottnawi <alexander.krasnoyarov@itgalaxy.company>
2018-02-09 19:34:06 +0300

    fix: provide error when parallel enable

--- src/uglify/worker.js
+++ src/uglify/worker.js
@@ -1,3 +1,4 @@
+import serializeError from 'serialize-error';
 import minify from './minify';
 
 module.exports = (options, callback) => {
@@ -7,7 +8,15 @@ module.exports = (options, callback) => {
     // eslint-disable-next-line no-new-func, no-param-reassign
     options = new Function(`'use strict'\nreturn ${options}`)();
 
-    callback(null, minify(options));
+    const result = minify(options);
+
+    // Communication channel employed by process.send() converts everything to JSON first.
+    // So custom object types (like Errors) can't possibly work, since object types aren't represented in JSON.
+    if (result.error) {
+      result.error = serializeError(result.error);
+    }
+
+    callback(null, result);
   } catch (errors) {
     callback(errors);
   }
