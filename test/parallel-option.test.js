b9afc6122eb7508f3d9c819b565377809895b0cd
evilebottnawi <sheo13666q@gmail.com>
2018-07-30 15:03:17 +0300

    refactor: `parallel` test

--- test/parallel-option.test.js
+++ test/parallel-option.test.js
@@ -2,7 +2,6 @@ import os from 'os';
 import workerFarm from 'worker-farm';
 import UglifyJsPlugin from '../src/index';
 import {
-  PluginEnvironment,
   createCompiler,
   compile,
   cleanErrorStack,
@@ -23,342 +22,87 @@ jest.mock('worker-farm', () => {
   return mock;
 });
 
-describe('when options.parallel', () => {
-  const assets = {
-    'test.js': {
-      source: () => 'function test(foo) { foo = 1; }',
-    },
-    'test1.js': {
-      source: () => 'function test1(foo) { foo = 1; }',
-    },
-    'test2.js': {
-      source: () => 'function test2(foo) { foo = 1; }',
-    },
-    'test3.js': {
-      source: () => 'function test3(foo) { foo = 1; }',
-    },
-  };
+describe('when applied with parallel option', () => {
+  let compiler;
 
-  describe('false', () => {
-    let eventBindings;
-    let eventBinding;
+  beforeEach(() => {
+    workerFarm.mockClear();
+    workerFarm.end.mockClear();
 
-    beforeEach(() => {
-      const pluginEnvironment = new PluginEnvironment();
-      const compilerEnv = pluginEnvironment.getEnvironmentStub();
-      compilerEnv.context = '';
-
-      const plugin = new UglifyJsPlugin({
-        parallel: false,
-      });
-      plugin.apply(compilerEnv);
-      eventBindings = pluginEnvironment.getEventBindings();
-    });
-
-    it('binds one event handler', () => {
-      expect(eventBindings.length).toBe(1);
-    });
-
-    describe('compilation handler', () => {
-      beforeEach(() => {
-        [eventBinding] = eventBindings;
-      });
-
-      it('binds to compilation event', () => {
-        expect(eventBinding.name).toBe('compilation');
-      });
-
-      describe('when called', () => {
-        let chunkPluginEnvironment;
-        let compilationEventBindings;
-        let compilationEventBinding;
-        let compilation;
-        let callback;
-
-        beforeEach(() => {
-          chunkPluginEnvironment = new PluginEnvironment();
-          compilation = chunkPluginEnvironment.getEnvironmentStub();
-          compilation.assets = Object.assign({}, assets);
-          compilation.errors = [];
-
-          workerFarm.mockClear();
-          workerFarm.end.mockClear();
-
-          eventBinding.handler(compilation);
-          compilationEventBindings = chunkPluginEnvironment.getEventBindings();
-        });
-
-        it('binds one event handler', () => {
-          expect(compilationEventBindings.length).toBe(1);
-        });
-
-        describe('optimize-chunk-assets handler', () => {
-          beforeEach(() => {
-            [compilationEventBinding] = compilationEventBindings;
-          });
-
-          it('binds to optimize-chunk-assets event', () => {
-            expect(compilationEventBinding.name).toEqual('optimize-chunk-assets');
-          });
-
-          it('only calls callback once', (done) => {
-            callback = jest.fn();
-            compilationEventBinding.handler([''], () => {
-              callback();
-              expect(callback.mock.calls.length).toBe(1);
-              done();
-            });
-          });
-
-          it('parallelization', (done) => {
-            compilationEventBinding.handler([{
-              files: ['test.js', 'test1.js', 'test2.js', 'test3.js'],
-            }], () => {
-              expect(workerFarm.mock.calls.length).toBe(0);
-              expect(workerFarm.end.mock.calls.length).toBe(0);
-
-              done();
-            });
-          });
-        });
-      });
-    });
-
-    it('matches snapshot', () => {
-      const compiler = createCompiler();
-      new UglifyJsPlugin({ parallel: false }).apply(compiler);
-
-      return compile(compiler).then((stats) => {
-        const errors = stats.compilation.errors.map(cleanErrorStack);
-        const warnings = stats.compilation.warnings.map(cleanErrorStack);
-
-        expect(errors).toMatchSnapshot('errors');
-        expect(warnings).toMatchSnapshot('warnings');
-
-        for (const file in stats.compilation.assets) {
-          if (Object.prototype.hasOwnProperty.call(stats.compilation.assets, file)) {
-            expect(stats.compilation.assets[file].source()).toMatchSnapshot(file);
-          }
-        }
-      });
+    compiler = createCompiler({
+      entry: {
+        one: `${__dirname}/fixtures/entry.js`,
+        two: `${__dirname}/fixtures/entry.js`,
+        three: `${__dirname}/fixtures/entry.js`,
+        four: `${__dirname}/fixtures/entry.js`,
+      },
     });
   });
 
-  describe('true', () => {
-    let eventBindings;
-    let eventBinding;
+  it('matches snapshot with false value', () => {
+    new UglifyJsPlugin({ parallel: false }).apply(compiler);
 
-    beforeEach(() => {
-      const pluginEnvironment = new PluginEnvironment();
-      const compilerEnv = pluginEnvironment.getEnvironmentStub();
-      compilerEnv.context = '';
+    return compile(compiler).then((stats) => {
+      const errors = stats.compilation.errors.map(cleanErrorStack);
+      const warnings = stats.compilation.warnings.map(cleanErrorStack);
 
-      const plugin = new UglifyJsPlugin({
-        parallel: true,
-      });
-      plugin.apply(compilerEnv);
-      eventBindings = pluginEnvironment.getEventBindings();
-    });
+      expect(workerFarm.mock.calls.length).toBe(0);
+      expect(workerFarm.end.mock.calls.length).toBe(0);
 
-    it('binds one event handler', () => {
-      expect(eventBindings.length).toBe(1);
-    });
+      expect(errors).toMatchSnapshot('errors');
+      expect(warnings).toMatchSnapshot('warnings');
 
-    describe('compilation handler', () => {
-      beforeEach(() => {
-        [eventBinding] = eventBindings;
-      });
-
-      it('binds to compilation event', () => {
-        expect(eventBinding.name).toBe('compilation');
-      });
-
-      describe('when called', () => {
-        let chunkPluginEnvironment;
-        let compilationEventBindings;
-        let compilationEventBinding;
-        let compilation;
-        let callback;
-
-        beforeEach(() => {
-          chunkPluginEnvironment = new PluginEnvironment();
-          compilation = chunkPluginEnvironment.getEnvironmentStub();
-          compilation.assets = Object.assign({}, assets);
-          compilation.warnings = [];
-          compilation.errors = [];
-
-          workerFarm.mockClear();
-          workerFarm.end.mockClear();
-
-          eventBinding.handler(compilation);
-          compilationEventBindings = chunkPluginEnvironment.getEventBindings();
-        });
-
-        it('binds one event handler', () => {
-          expect(compilationEventBindings.length).toBe(1);
-        });
-
-        describe('optimize-chunk-assets handler', () => {
-          beforeEach(() => {
-            [compilationEventBinding] = compilationEventBindings;
-          });
-
-          it('binds to optimize-chunk-assets event', () => {
-            expect(compilationEventBinding.name).toEqual('optimize-chunk-assets');
-          });
-
-          it('only calls callback once', (done) => {
-            callback = jest.fn();
-            compilationEventBinding.handler([''], () => {
-              callback();
-              expect(callback.mock.calls.length).toBe(1);
-              done();
-            });
-          });
-
-          it('parallelization', (done) => {
-            compilationEventBinding.handler([{
-              files: ['test.js', 'test1.js', 'test2.js', 'test3.js'],
-            }], () => {
-              expect(workerFarm.mock.calls.length).toBe(1);
-              expect(workerFarm.mock.calls[0][0].maxConcurrentWorkers).toBe(os.cpus().length - 1);
-              expect(workerFarmMock.mock.calls.length).toBe(4);
-              expect(workerFarm.end.mock.calls.length).toBe(1);
-
-              done();
-            });
-          });
-        });
-      });
-    });
-
-    it('matches snapshot', () => {
-      const compiler = createCompiler();
-      new UglifyJsPlugin({ parallel: true }).apply(compiler);
-
-      return compile(compiler).then((stats) => {
-        const errors = stats.compilation.errors.map(cleanErrorStack);
-        const warnings = stats.compilation.warnings.map(cleanErrorStack);
-
-        expect(errors).toMatchSnapshot('errors');
-        expect(warnings).toMatchSnapshot('warnings');
-
-        for (const file in stats.compilation.assets) {
-          if (Object.prototype.hasOwnProperty.call(stats.compilation.assets, file)) {
-            expect(stats.compilation.assets[file].source()).toMatchSnapshot(file);
-          }
+      for (const file in stats.compilation.assets) {
+        if (Object.prototype.hasOwnProperty.call(stats.compilation.assets, file)) {
+          expect(stats.compilation.assets[file].source()).toMatchSnapshot(file);
         }
-      });
+      }
     });
   });
 
-  describe('number', () => {
-    let eventBindings;
-    let eventBinding;
+  it('matches snapshot with true value', () => {
+    new UglifyJsPlugin({ parallel: true }).apply(compiler);
 
-    beforeEach(() => {
-      const pluginEnvironment = new PluginEnvironment();
-      const compilerEnv = pluginEnvironment.getEnvironmentStub();
-      compilerEnv.context = '';
+    return compile(compiler).then((stats) => {
+      const errors = stats.compilation.errors.map(cleanErrorStack);
+      const warnings = stats.compilation.warnings.map(cleanErrorStack);
 
-      const plugin = new UglifyJsPlugin({
-        parallel: 2,
-      });
-      plugin.apply(compilerEnv);
-      eventBindings = pluginEnvironment.getEventBindings();
-    });
+      expect(workerFarm.mock.calls.length).toBe(1);
+      expect(workerFarm.mock.calls[0][0].maxConcurrentWorkers).toBe(os.cpus().length - 1);
+      expect(workerFarmMock.mock.calls.length).toBe(Object.keys(stats.compilation.assets).length);
+      expect(workerFarm.end.mock.calls.length).toBe(1);
 
-    it('binds one event handler', () => {
-      expect(eventBindings.length).toBe(1);
-    });
+      expect(errors).toMatchSnapshot('errors');
+      expect(warnings).toMatchSnapshot('warnings');
 
-    describe('compilation handler', () => {
-      beforeEach(() => {
-        [eventBinding] = eventBindings;
-      });
-
-      it('binds to compilation event', () => {
-        expect(eventBinding.name).toBe('compilation');
-      });
-
-      describe('when called', () => {
-        let chunkPluginEnvironment;
-        let compilationEventBindings;
-        let compilationEventBinding;
-        let compilation;
-        let callback;
-
-        beforeEach(() => {
-          chunkPluginEnvironment = new PluginEnvironment();
-          compilation = chunkPluginEnvironment.getEnvironmentStub();
-          compilation.assets = Object.assign({}, assets);
-          compilation.warnings = [];
-          compilation.errors = [];
-
-          workerFarm.mockClear();
-          workerFarm.end.mockClear();
-
-          eventBinding.handler(compilation);
-          compilationEventBindings = chunkPluginEnvironment.getEventBindings();
-        });
-
-        it('binds one event handler', () => {
-          expect(compilationEventBindings.length).toBe(1);
-        });
-
-        describe('optimize-chunk-assets handler', () => {
-          beforeEach(() => {
-            [compilationEventBinding] = compilationEventBindings;
-          });
-
-          it('binds to optimize-chunk-assets event', () => {
-            expect(compilationEventBinding.name).toEqual('optimize-chunk-assets');
-          });
-
-          it('only calls callback once', (done) => {
-            callback = jest.fn();
-            compilationEventBinding.handler([''], () => {
-              callback();
-              expect(callback.mock.calls.length).toBe(1);
-              done();
-            });
-          });
-
-          it('parallelization', (done) => {
-            compilationEventBinding.handler([{
-              files: ['test.js', 'test1.js', 'test2.js', 'test3.js'],
-            }], () => {
-              expect(workerFarm.mock.calls.length).toBe(1);
-              // Appveyor give only one core
-              expect(workerFarm.mock.calls[0][0].maxConcurrentWorkers)
-                .toBe(Math.min(Number(2) || 0, os.cpus().length - 1));
-              expect(workerFarmMock.mock.calls.length).toBe(4);
-              expect(workerFarm.end.mock.calls.length).toBe(1);
-
-              done();
-            });
-          });
-        });
-      });
-    });
-
-    it('matches snapshot', () => {
-      const compiler = createCompiler();
-      new UglifyJsPlugin({ parallel: true }).apply(compiler);
-
-      return compile(compiler).then((stats) => {
-        const errors = stats.compilation.errors.map(cleanErrorStack);
-        const warnings = stats.compilation.warnings.map(cleanErrorStack);
-
-        expect(errors).toMatchSnapshot('errors');
-        expect(warnings).toMatchSnapshot('warnings');
-
-        for (const file in stats.compilation.assets) {
-          if (Object.prototype.hasOwnProperty.call(stats.compilation.assets, file)) {
-            expect(stats.compilation.assets[file].source()).toMatchSnapshot(file);
-          }
+      for (const file in stats.compilation.assets) {
+        if (Object.prototype.hasOwnProperty.call(stats.compilation.assets, file)) {
+          expect(stats.compilation.assets[file].source()).toMatchSnapshot(file);
         }
-      });
+      }
+    });
+  });
+
+  it('matches snapshot with 2 value (number)', () => {
+    new UglifyJsPlugin({ parallel: 2 }).apply(compiler);
+
+    return compile(compiler).then((stats) => {
+      const errors = stats.compilation.errors.map(cleanErrorStack);
+      const warnings = stats.compilation.warnings.map(cleanErrorStack);
+
+      expect(workerFarm.mock.calls.length).toBe(1);
+      expect(workerFarm.mock.calls[0][0].maxConcurrentWorkers).toBe(2);
+      expect(workerFarmMock.mock.calls.length).toBe(Object.keys(stats.compilation.assets).length);
+      expect(workerFarm.end.mock.calls.length).toBe(1);
+
+      expect(errors).toMatchSnapshot('errors');
+      expect(warnings).toMatchSnapshot('warnings');
+
+      for (const file in stats.compilation.assets) {
+        if (Object.prototype.hasOwnProperty.call(stats.compilation.assets, file)) {
+          expect(stats.compilation.assets[file].source()).toMatchSnapshot(file);
+        }
+      }
     });
   });
 });
